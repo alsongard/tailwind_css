0fe903e0437fb8b0a0f01ef627685f5b4b976d63
alsongard <alsongard@gmail.com>
2024-09-14 09:46:53 -0400

    selecting customized style value

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -3,9 +3,9 @@ function App() {
   return (
     <section>
       <header className="bg-rose-500 border-b-2 border-sky-500" >
-        <h1 className="text-2xl font-bold  text-white text-left">Hello world!</h1>
+        <h1 className="text-4xl text-teal-400 font-bold text-left">Hello world!</h1>
       </header>
-      <p>This is my Fight Song</p>
+      <p className="text-4xl text-newer-500">This is my Fight Song</p>
     </section>
 
   );
