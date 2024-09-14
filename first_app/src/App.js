807f6b2262f02e94e12365cf52661396d01864c7
alsongard <alsongard@gmail.com>
2024-09-14 10:14:18 -0400

    applying css style based on screen size

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -6,6 +6,11 @@ function App() {
         <h1 className="text-4xl text-teal-400 font-bold text-left">Hello world!</h1>
       </header>
       <p className="text-4xl text-newer-500">This is my Fight Song</p>
+
+      <h3>Alter size depending on screen</h3>
+      <p>Take screen customization and add to you tailwindcss.config.js file</p>
+      <p className="text-xs lg:text-3xl">Apply to me! == (means this sentence)</p>
+
     </section>
 
   );
